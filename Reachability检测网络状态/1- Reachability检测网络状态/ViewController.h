3b0ed38c6c47959cc2a240fa55684df3db0bf82e
test <422538722@qq.com>
2017-09-15 13:16:40 +0800

    12.8

--- Reachability检测网络状态/1- Reachability检测网络状态/ViewController.h
+++ Reachability检测网络状态/1- Reachability检测网络状态/ViewController.h
@@ -1,10 +1,4 @@
-//
-//  ViewController.h
-//  1- Reachability检测网络状态
-//
-//  Created by 史昕 on 16/4/21.
-//  Copyright © 2016年 HC. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
--- Reachability检测网络状态/1- Reachability检测网络状态/ViewController.m
+++ Reachability检测网络状态/1- Reachability检测网络状态/ViewController.m
@@ -1,10 +1,4 @@
-//
-//  ViewController.m
-//  1- Reachability检测网络状态
-//
-//  Created by 史昕 on 16/4/21.
-//  Copyright © 2016年 HC. All rights reserved.
-//
+
 
 #import "ViewController.h"
 #import "Reachability.h"
